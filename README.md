ac695041f8f9552262d894f0ad0ef7c188c43b30
Manoj M Wagle <74168582+manojmw@users.noreply.github.com>
2024-07-28 11:06:29 +1000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Hydra
+# Interpretable deep generative learning of cell identity paired with automated annotation for single-cell multi-omics
+
+</br>
+
+
+![Hydra_Framework_v6](https://github.com/manojmw/Hydra/assets/74168582/d2879af6-9a3a-4db8-b818-e6e1ccd71a6c)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,57 @@
+# Manoj M Wagle (USydney; CMRI)
+##############################################
+
+from setuptools import setup, find_namespace_packages
+import subprocess
+import sys
+
+def install_numpy():
+    try:
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'numpy'])
+    except subprocess.CalledProcessError:
+        raise RuntimeError("Failed to install numpy")
+
+# Install numpy first
+install_numpy()
+
+setup(
+    name='hydra',
+    version='0.1.0',
+    packages=find_namespace_packages(),
+    install_requires=[
+        'numpy',
+        'anndata',
+        'captum',
+        'h5py',
+        'libpng-bins',
+        'libtiff',
+        'torch',
+        'torchvision',
+        'torchaudio',
+        'pandas',
+        'scipy',
+        'scikit-learn',
+        'scanpy',
+        'tqdm',
+    ],
+    entry_points={
+        'console_scripts': [
+            'hydra=hydra.Hydra:main',
+        ],
+    },
+    include_package_data=True,
+    description='Thank you for using Hydra 😄, an interpretable deep generative tool for single-cell multiomics. Please refer to the full documentation available at [xxx] for detailed usage instructions. If you encounter any issues running the tool - Please open an issue on Github, and we will get back to you as soon as possible!!',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/Hydra',
+    author='Manoj M Wagle',
+    author_email='mwag8019@uni.sydney.edu.au',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Computer Science :: Bioinformatics/Computational Biology',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8'
+    ],
+)
