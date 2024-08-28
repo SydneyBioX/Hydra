36d80d7219a2600d0c11b689ad99b8c084d93af9
Manoj M Wagle <74168582+manojmw@users.noreply.github.com>
2024-08-29 09:54:21 +1000

    Update Annotation.py

--- hydra/Annotation.py
+++ hydra/Annotation.py
@@ -29,7 +29,7 @@ parser.add_argument('--seed', type = int, default = 42, help ='seed')
 parser.add_argument('--proccessedata_dir', metavar = 'DIR', default = os.getcwd(), help = 'Path to the directory containing processed data. Default: Current working directory')  
 parser.add_argument('--balanceddata_dir', metavar = 'DIR', default = os.getcwd(), help = 'Path to the directory containing balanced data. Default: Current working directory')
 parser.add_argument('--fs_dir', metavar = 'DIR', default = os.getcwd(), help = 'Path to the directory containing feature selection results. Default: Current working directory')
-parser.add_argument('--modality', default='rna', required=True, choices=['rna', 'adt', 'atac'], help='Input data modality. Default: `rna`')
+parser.add_argument('--modality', default='rna', choices=['rna', 'adt', 'atac'], help='Input data modality. Default: `rna`')
 
 # For training
 parser.add_argument('--batch_sizeanno', type = int, default = 64, help = 'batch size')
