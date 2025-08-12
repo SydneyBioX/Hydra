b38626b82cf6fa752e39541885cebd1e32818776
manojmw <manojmonmax@gmail.com>
2025-08-13 00:19:12 +1000

    Prep v0.1.0: metadata and build system

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name="hydra-tools",
-    version="0.1.7",
+    version="0.1.0",
     packages=find_namespace_packages(),
     install_requires=[
         'anndata',
@@ -36,10 +36,10 @@ setup(
         ],
     },
     include_package_data=True,
-    description='Thank you for using Hydra 😄, an interpretable deep generative tool for single-cell omics. Please refer to the full documentation available at [xxx] for detailed usage instructions. If you encounter any issues running the tool - Please open an issue on Github, and we will get back to you as soon as possible!!',
+    description='Thank you for using Hydra 😄, an interpretable deep generative tool for single-cell omics. Please refer to the full documentation available at https://sydneybiox.github.io/Hydra/ for detailed usage instructions. If you encounter any issues running the tool - Please open an issue on Github, and we will get back to you as soon as possible!!',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/manojmw/Hydra',
+    url='https://github.com/SydneyBioX/Hydra',
     author='Manoj M Wagle',
     author_email='mwag8019@uni.sydney.edu.au',
     license='MIT',
